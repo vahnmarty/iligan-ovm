8317e7c95517feb360df505db40420ebb1c17a24
Vahn <vahnmarty@gmail.com>
2022-10-01 23:33:38 +0800

    widgets

--- app/Http/Livewire/Logistics/Dashboard.php
+++ app/Http/Livewire/Logistics/Dashboard.php
@@ -7,20 +7,28 @@ use App\Models\RequisitionItem;
 
 class Dashboard extends Component
 {
-    public $today_widgets = [1,2,3,4];
+    public $today_widgets = [], $tomorrow_widgets = [];
     
     public function render()
     {
+        $this->getTodayMaterials();
+        $this->getTomorrowMaterials();
+
         return view('livewire.logistics.dashboard')->layout('layouts.kiosk');
     }
 
     public function mount()
     {
-        $this->getTodayMaterials();
+        
     }
 
     public function getTodayMaterials()
     {
-        $this->today_widgets = RequisitionItem::with('material', 'parent')->get();
+        $this->today_widgets = RequisitionItem::with('material', 'parent')->whereDate('date_start', date('Y-m-d'))->get();
+    }
+
+    public function getTomorrowMaterials()
+    {
+        $this->tomorrow_widgets = RequisitionItem::with('material', 'parent')->whereDate('date_start', date('Y-m-d', strtotime('+1 day')))->get();
     }
 }
--- resources/views/livewire/logistics/dashboard.blade.php
+++ resources/views/livewire/logistics/dashboard.blade.php
@@ -1,4 +1,4 @@
-<div class="min-h-screen text-white bg-blue-500 h-100">
+<div class="min-h-screen pb-64 text-white bg-blue-500 h-100" wire:poll>
     <header class="flex items-end justify-between px-8 py-10 pb-8 mb-8 border-b border-white">
         <h1 class="text-4xl font-bold">OVM | Logistics</h1>
         <p class="text-2xl font-bold">{{ now()->format('F d, Y h:i a') }}</p>
@@ -14,7 +14,7 @@
                     <div class="ml-4">
                         <h4 class="text-lg font-bold">{{ $item->material->name }}</h4>
                         <p>x{{ $item->quantity }}</p>
-                        <p class="mt-4"><strong>Reference:</strong> {{ $item->parent->control_number }}</p>
+                        <p class="mt-4"><strong>Reference:</strong> #{{ $item->parent->id }}</p>
                     </div>
                 </div>
                 @endforeach
@@ -24,13 +24,13 @@
         <section>
             <h3 class="text-2xl font-bold">TOMORROW</h3>
             <div class="grid grid-cols-4 gap-16 mt-4">
-                @foreach($today_widgets as $item)
+                @foreach($tomorrow_widgets as $item)
                 <div class="flex px-4 py-6 text-gray-900 bg-white rounded-md shadow-sm">
                     @svg($item->material->ui_icon, ['class' => 'w-12 h-12 text-gray-500'])
                     <div class="ml-4">
                         <h4 class="text-lg font-bold">{{ $item->material->name }}</h4>
                         <p>x{{ $item->quantity }}</p>
-                        <p class="mt-4"><strong>Reference:</strong> {{ $item->parent->control_number }}</p>
+                        <p class="mt-4"><strong>Reference:</strong> #{{ $item->parent->id }}</p>
                     </div>
                 </div>
                 @endforeach
