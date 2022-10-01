bbf07af852a3d1af1ae3d55436b3e74b69aa47e7
Vahn <vahnmarty@gmail.com>
2022-10-01 23:44:20 +0800

    offline mode

--- resources/views/livewire/logistics/dashboard.blade.php
+++ resources/views/livewire/logistics/dashboard.blade.php
@@ -1,13 +1,22 @@
 <div class="min-h-screen pb-64 text-white bg-blue-500 h-100" wire:poll>
+
+    <div wire:offline>
+        <div class="fixed top-0 left-0 right-0 z-50">
+            <div class="flex justify-center w-full py-4 text-white bg-red-700">
+                <p>You are now offline.</p>
+            </div>
+        </div>
+    </div>
+
     <header class="flex items-end justify-between px-8 py-10 pb-8 mb-8 border-b border-white">
         <h1 class="text-4xl font-bold">OVM | Logistics</h1>
-        <p class="text-2xl font-bold">{{ now()->format('F d, Y h:i a') }}</p>
+        <p class="text-2xl font-bold">{{ now()->format('F d, Y h:i:s a') }}</p>
     </header>
 
     <div class="px-8 mt-8 space-y-16">
         <section>
             <h3 class="text-2xl font-bold">TODAY</h3>
-            <div class="grid grid-cols-4 gap-16 mt-4">
+            <div class="grid grid-cols-3 gap-16 mt-4 lg:grid-cols-3 xl:grid-cols-4">
                 @foreach($today_widgets as $item)
                 <div class="flex px-4 py-6 text-gray-900 bg-white rounded-md shadow-sm">
                     @svg($item->material->ui_icon, ['class' => 'w-12 h-12 text-gray-500'])
@@ -23,7 +32,7 @@
 
         <section>
             <h3 class="text-2xl font-bold">TOMORROW</h3>
-            <div class="grid grid-cols-4 gap-16 mt-4">
+            <div class="grid grid-cols-3 gap-16 mt-4 lg:grid-cols-3 xl:grid-cols-4">
                 @foreach($tomorrow_widgets as $item)
                 <div class="flex px-4 py-6 text-gray-900 bg-white rounded-md shadow-sm">
                     @svg($item->material->ui_icon, ['class' => 'w-12 h-12 text-gray-500'])
